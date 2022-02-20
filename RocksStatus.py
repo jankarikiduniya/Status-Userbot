8b3c8263b75b1c7dcd1c7f09d446bae4cd169aac
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2022-02-20 10:18:24 +0500

    Bor Status

---
+++ RocksStatus.py
@@ -0,0 +1,62 @@
+# A Property Of Rocks Indian Largest Chatting Group
+# Without Credit (Mother Fucker)
+# Rocks © @Dr_Asad_Ali © Rocks
+# Copyright (C) 2021 By @Itz_VeNom_xD 
+# Copyright (C) 2021 By @Dr_Asad_Ali
+# Copyright (C) 2021 By @HarshitSharma361
+
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+import asyncio
+import datetime
+import pytz
+import os
+
+app = Client(
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"],
+    session_name = os.environ["SESSION_NAME"]
+)
+TIME_ZONE = os.environ["TIME_ZONE"]
+BOT_LIST = [i.strip() for i in os.environ.get("BOT_LIST").split(' ')]
+CHANNEL_OR_GROUP_ID = int(os.environ["CHANNEL_OR_GROUP_ID"])
+MESSAGE_ID = int(os.environ["MESSAGE_ID"])
+BOT_ADMIN_IDS = [int(i.strip()) for i in os.environ.get("BOT_ADMIN_IDS").split(' ')]
+
+async def main_idncoder():
+    async with app:
+            while True:
+                print("Checking The System...")
+                GET_CHANNEL_OR_GROUP = await app.get_chat(int(CHANNEL_OR_GROUP_ID))
+                CHANNEL_OR_GROUP_NAME = GET_CHANNEL_OR_GROUP.title
+                CHANNEL_OR_GROUP_TYPE = GET_CHANNEL_OR_GROUP.type
+                xxx_idncoder = f"📊 **<u>LIVE BOT STATUS</u>**\n\n**💬 {CHANNEL_OR_GROUP_TYPE}**: {CHANNEL_OR_GROUP_NAME}"
+                for bot in BOT_LIST:
+                    try:
+                        yyy_idncoder = await app.send_message(bot, "/start")
+                        aaa = yyy_idncoder.message_id
+                        await asyncio.sleep(7)
+                        zzz_idncoder = await app.get_history(bot, limit = 1)
+                        for ccc in zzz_idncoder:
+                            bbb = ccc.message_id
+                        if aaa == bbb:
+                            xxx_idncoder += f"\n\n🤖 **BOT**: @{bot}\n🔴 **STATUS**: Down ❌"
+                            for bot_admin_id in BOT_ADMIN_IDS:
+                                try:
+                                    await app.send_message(int(bot_admin_id), f"🚨 **Beep! Beep! @{bot} is down** ❌")
+                                except Exception:
+                                    pass
+                            await app.read_history(bot)
+                        else:
+                            xxx_idncoder += f"\n\n🤖 **BOT**: @{bot}\n🟢 **STATUS**: Alive ✅"
+                            await app.read_history(bot)
+                    except FloodWait as e:
+                        await asyncio.sleep(e.x)            
+                time = datetime.datetime.now(pytz.timezone(f"{TIME_ZONE}"))
+                last_update = time.strftime(f"%d %b %Y at %I:%M %p")
+                xxx_idncoder += f"\n\n✔️ Last checked on: {last_update} ({TIME_ZONE})\n\n<i>♻️ Updates every 45 minutes - Powered By @AsadSupport</i>"
+                await app.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID, xxx_teletips)
+                print(f"Last checked on: {last_update}")                
+                await asyncio.sleep(1500)
+                        
+app.run(main_idncoder())
